fff9e898bd85795af324313e96597c367e08e56f
Lari Kovanen <lari@kovanen.se>
2017-03-03 17:23:12 +0100

    Fixed randomizer and text output.

--- bingo.rb
+++ bingo.rb
@@ -1,20 +1,20 @@
 #!/usr/bin/ruby
 
-bingonumber = rand(75)
-
+bingonumber = rand(75)+1
 case bingonumber
     when 1..9
-        puts "Bertil #{bingonumber} enkel #{bingonumber}"
+        puts "Bertil #{bingonumber} enkel #{bingonumber}:a"
     when 10..15
-        puts "Bertil #{bingonumber}"
+        puts "Bertil #{bingonumber} #{bingonumber.to_s[0]}:a #{bingonumber.to_s[1]}:a"
     when 16..30
-        puts "Ivar #{bingonumber}"
+        puts "Ivar #{bingonumber} #{bingonumber.to_s[0]}:a #{bingonumber.to_s[1]}:a"
     when 31..45
-        puts "Niklas #{bingonumber}"
+        puts "Niklas #{bingonumber} #{bingonumber.to_s[0]}:a #{bingonumber.to_s[1]}:a"
     when 46..60
-        puts "Gustav #{bingonumber}"
+        puts "Gustav #{bingonumber} #{bingonumber.to_s[0]}:a #{bingonumber.to_s[1]}:a"
     when 61..75
-        puts "Olof #{bingonumber}"
+        puts "Olof #{bingonumber} #{bingonumber.to_s[0]}:a #{bingonumber.to_s[1]}:a"
     else
         puts "Something else #{bingonumber}"
 end
+
